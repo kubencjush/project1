aae9377933efff03bb0afb7957959fb3e04c2242
kubencjush <jakubkg@gmail.com>
2018-01-07 15:41:51 +0100

    first commit

---
+++ src/main/java/com/kuba/Main.java
@@ -0,0 +1,12 @@
+package com.kuba;
+
+public class Main {
+
+    public static void main (String[] arg) {
+        System.out.println("Hello");
+
+        System.out.println("Hello2");
+        System.out.println("Hello3");
+        System.out.println("Hello4" +arg[0]+arg[1]);
+    }
+}
